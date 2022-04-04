439496a7afaaa8d9984a193a85d70528c1467590
Sovran Martet <sovran.martet@gmail.com>
2022-04-04 15:33:54 +0200

    Recent changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from "react";
 
@@ -6,17 +5,23 @@ function App() {
   const [companies, setCompanies] = useState([]);
   const url = "http://localhost:5000/api"
   useEffect(() => {
-    fetch(url, {
-      method: "GET",
-      mode: "no-cors"
-    })
+    fetch(url)
     .then(result => result.json())
-    .then(data => setCompanies(data))
-  })
+    .then(data => setCompanies(data.operators))
+  }, [])
+
+  const privateCompanies = companies.companies
+  const agencies = companies.agencies;
+  console.log(privateCompanies);
 
   return (
     <main>
       <h1>Stellar Sailing</h1>
+{/*       {
+        privateCompanies.map((company) => (
+          <h1>{company.name}</h1>
+        ))
+      } */}
     </main>
   );
 }
