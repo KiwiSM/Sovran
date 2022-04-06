3f91904de2093aa73aaeda657280ee7d36f0f8fa
Sovran Martet <sovran.martet@gmail.com>
2022-04-06 21:58:26 +0200

    Recent

--- frontend/src/reducers/operatorReducer.js
+++ frontend/src/reducers/operatorReducer.js
@@ -2,8 +2,8 @@ import { operator } from "../globalStore";
 
 const operatorReducer = (state = operator, action) => {
     switch(action.type) {
-        case "ADD_OPERATOR": 
-            return state = action.payload
+        case "ADD_OPERATOR":
+            return [...state, action.payload]
         case "REMOVE_OPERATOR":
             return "Blue Origin"
         default: 
